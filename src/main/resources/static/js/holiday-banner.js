f79fa455cb4237f9859282057704b087d080c541
Tai Nguyen <ntainguyen.it@gmail.com>
2026-09-04 20:04:45 +0700

    Add typewriter effect to greetings, fix missing tailwind classes for share button and feedback form

--- src/main/resources/static/js/holiday-banner.js
+++ src/main/resources/static/js/holiday-banner.js
@@ -1,18 +1,30 @@
-﻿(function() {
+(function() {
     const today = new Date();
     const greetingEl = document.getElementById('greeting-text');
     if (greetingEl) {
         const hour = today.getHours();
         let greeting = '';
         if (hour >= 0 && hour < 11) {
-            greeting = 'Chào buổi sáng! ☕';
+            greeting = 'Chào buổi sáng, chúc bạn ngày mới tốt lành!';
         } else if (hour >= 11 && hour < 14) {
-            greeting = 'Chào buổi trưa! 🍲';
+            greeting = 'Chào buổi trưa, chúc bạn ngày mới tốt lành!';
         } else if (hour >= 14 && hour < 18) {
-            greeting = 'Chào buổi chiều! 🌤️';
+            greeting = 'Chào buổi chiều, chúc bạn ngày mới tốt lành!';
         } else {
-            greeting = 'Chào buổi tối! 🌙';
+            greeting = 'Chào buổi tối, chúc bạn ngày mới tốt lành!';
         }
-        greetingEl.innerText = greeting;
+        
+        greetingEl.innerHTML = '';
+        let i = 0;
+        function typeWriter() {
+            if (i < greeting.length) {
+                greetingEl.innerHTML += greeting.charAt(i);
+                i++;
+                setTimeout(typeWriter, 50);
+            } else {
+                greetingEl.innerHTML += '<span class="animate-pulse">|</span>';
+            }
+        }
+        typeWriter();
     }
 })();
